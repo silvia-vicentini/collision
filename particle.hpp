51a1ed2a1be1d3bc2cbf7d2a09cdc01f9e5ce309
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2024-01-02 13:50:34 +0100

    Update particle.hpp

--- particle.hpp
+++ particle.hpp
@@ -3,28 +3,33 @@
 #include "resonancetype.hpp"
 #include <vector>
 
-// scarica root su ubuntu!
-
 class Particle
 {
 private:
+    // Container of the differet types of particles generated
     static std::vector<ParticleType *> fParticleType;
+
     static const int fMaxNumParticleType = 10;
     int fIndex;
     const char *fName;
     double fPx;
     double fPy;
     double fPz;
+
+    // Funtion to define the corresponding type of the particle above those incuded in fParticleType
     static int FindParticle(const char *);
+
+    // Function used in Decay2Body
     void Boost(double, double, double);
 
 public:
-    // costruttore
+    // Constructor
     Particle(const char *, double Px = 0, double Py = 0, double Pz = 0);
 
     // Default constructor
     Particle();
 
+    // Getter methods
     double GetPx() const;
     double GetPy() const;
     double GetPz() const;
@@ -33,18 +38,25 @@ public:
     int GetCharge() const;
     static int GetSize();
 
+    // Function to calculate the energy of a particle
     double Energy() const;
-    double InvMass(Particle &);
 
+    // Function to calculate the invariant mass of a particle fron the products of decayment
+    double InvMass(Particle &) const;
+
+    // Function to add a type of particle to fParticleType
     static void AddParticleType(const char *, const double, const int, double width = 0);
 
+    // Setter methods
     void SetIndex(const int);
     void SetIndex(const char *);
     void SetP(double, double, double);
 
+    // Printer methods
     static void PrintfParticleType();
     void PrintParticle() const;
 
+    // Function to set the products of decayment
     int Decay2Body(Particle &, Particle &) const;
 };
 
